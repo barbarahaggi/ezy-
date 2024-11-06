07bb7d892a26177a4d8b6b48c536b0139d0ee3de
Humberto Vitalino <humbertodark@outlook.com>
2024-11-05 21:20:18 -0300

    feat: cliente java bean

---
+++ src/main/java/br/com/fiap/fintech/model/Cliente.java
@@ -0,0 +1,95 @@
+package br.com.fiap.fintech.model;
+
+import br.com.fiap.fintech.util.Criptografia;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private LocalDate nasc;
+    private String endereco;
+    private String telefone;
+    private String senha;
+
+    public Cliente(int id, String nome, String cpf, String email, String senha, String endereco, String telefone, LocalDate nasc) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.nasc = nasc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        try {
+            this.senha = Criptografia.criptografar(senha);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public LocalDate getNasc() {
+        return nasc;
+    }
+
+    public void setNasc(LocalDate nasc) {
+        this.nasc = nasc;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
