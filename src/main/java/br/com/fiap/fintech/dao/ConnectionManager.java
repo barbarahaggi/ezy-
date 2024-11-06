0abb6ddfdbd583ccd27e91265f5708176e8b9e37
Humberto Vitalino <humbertodark@outlook.com>
2024-11-05 21:19:25 -0300

    feat: connectionManager

---
+++ src/main/java/br/com/fiap/fintech/dao/ConnectionManager.java
@@ -0,0 +1,30 @@
+package br.com.fiap.fiapstore.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static ConnectionManager connectionManager;
+    public static ConnectionManager getInstance() {
+        if (connectionManager == null) {
+            connectionManager = new ConnectionManager();
+        }
+        return connectionManager;
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","user","password");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+}
