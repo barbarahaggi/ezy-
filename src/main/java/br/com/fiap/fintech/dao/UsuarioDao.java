5df15f6e87e4ead3e67643a4479d2768c74baa18
Humberto Vitalino <humbertodark@outlook.com>
2024-11-08 19:18:50 -0300

    feat: user

---
+++ src/main/java/br/com/fiap/fintech/dao/UsuarioDao.java
@@ -0,0 +1,7 @@
+package br.com.fiap.fintech.dao;
+
+import br.com.fiap.fintech.model.Usuario;
+
+public interface UsuarioDao {
+    boolean validarUsuario(Usuario usuario);
+}
--- src/main/java/br/com/fiap/fintech/factory/DaoFactory.java
+++ src/main/java/br/com/fiap/fintech/factory/DaoFactory.java
@@ -1,4 +1,10 @@
 package br.com.fiap.fintech.factory;
 
+import br.com.fiap.fintech.dao.UsuarioDao;
+import br.com.fiap.fintech.dao.impl.OracleUsuarioDao;
+
 public class DaoFactory {
+    public static UsuarioDao getUsuarioDao() {
+        return new OracleUsuarioDao();
+    }
 }
